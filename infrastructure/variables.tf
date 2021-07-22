ab66116acf12e2863a14bb1fbc7902f950616487
Leandro-A-B <leandro.bueno@df.ufscar.br>
2021-07-22 16:21:30 -0300

    atualização descrição das variaveis e numero conta

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,19 @@
 variable "aws_region" {
+  description = "Definição da região"
   default = "us-east-1"
 }
 
 variable "base_bucket_name" {
+  description = "Nome do bucket"
   default = "my-bucket"
 }
 
 variable "account" {
-  default = 123456789
+  description = "Número da conta para execução"
+  default = 127012818163
 }
 
 variable "lambda_function_name" {
+  description = "Nome da Lambda Function"
   default = "my-lambda"
 }

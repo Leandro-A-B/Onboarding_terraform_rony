ce8ef48f6f1516df9e7d743e01fadf932654f739
Leandro-A-B <leandro.bueno@df.ufscar.br>
2021-07-23 10:27:55 -0300

    Update no script da function

--- etl/lambda_function.py
+++ etl/lambda_function.py
@@ -1,12 +1,31 @@
 import boto3
 import os
+import urllib.request
+
+
+# Path do arquivo - Constante
+file_name = "/tmp/titanic.csv"
+object_name = "raw-data/titanic/titanic.csv"
+
+
+# Pegar dados
+def get_data():
+    url = "https://raw.githubusercontent.com/neylsoncrepalde/titanic_data_with_semicolon/main/titanic.csv"
+    urllib.request.urlretrieve(url, file_name)
+    return True
+
 
 def handler(event, context):
 
     bucket_name = os.environ['BUCKET']
     region = os.environ['REGION']
 
+    res = get_data()
+
+    s3 = boto3.client("s3")
+    s3.upload_file(file_name, bucket_name, object_name)
+
     return {
         'statusCode': 200,
-        'body': f'Hello World with bucket {bucket_name} on region {region}!'
+        'body': f'Subiram os arquivos no {bucket_name} na região {region}!'
     }

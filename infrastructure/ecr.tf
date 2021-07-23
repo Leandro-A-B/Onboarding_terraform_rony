3a788e66f59132e06b6159d658e6082157204d3f
Leandro-A-B <leandro.bueno@df.ufscar.br>
2021-07-23 12:16:02 -0300

    update dos fatores para deploy do datalake

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repo" {
-  name                 = "etl-job"
+  name                 = "etl-job-leandro-bueno"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
